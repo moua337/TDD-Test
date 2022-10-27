1ba0fabc257984fce71bb563d8749a6148b8f2f7
Ethan Moua <112973340+moua337@users.noreply.github.com>
2022-10-27 08:42:18 -0400

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,10 @@
+class Animal:
+    def __init__(self):
+        self.type = ([])
+        self.size = ([])
+        self.age = ([])
+        self.name = ([])
+    def __init__(speak):
+        pass
+    def __init__(desribe):
+        pass
