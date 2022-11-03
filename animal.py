a6443d78daa6d8fefacd33f0a9b1dcde46ab9e09
Ethan Moua <112973340+moua337@users.noreply.github.com>
2022-11-03 13:57:19 -0400

    Update animal.py

--- animal.py
+++ animal.py
@@ -1,10 +1,32 @@
 class Animal:
-    def __init__(self):
-        self.type = ""
+    def __init__(self, ctype):
+        self.type = ctype
         self.size = ""
         self.age = 0
-        self.name = ""
-    def speak(self):
-        pass
+        self.name = "" 
+
+        if self.type == 'cat':
+            self.name = 'Seymour'
+        if self.type == 'dog':
+            self.name = 'Spot'
+        
+   
+    def speak(self): 
+        if self.type == 'cat':
+            if self.size == 'small':
+                return 'meow'
+            elif self.size != 'small':
+                return 'MEOW!'
+        if self.type == 'dog':
+            if self.size == 'small':
+                return 'bow wow'
+            elif self.size == 'medium':
+                return 'Ruff ruff'
+            else:
+                return 'arf arf' 
+
     def describe(self):
-        pass
+        if self.age < 10:
+            return f"{self.name} is young"
+        else:
+            return f"{self.name} is old"
